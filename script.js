ebc4d76987a560014e81b2834ae83fbdd1085b82
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-10-21 18:42:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const btn = document.getElementById("btn");
+btn.addEventListener('click',delayfun);
+async function delayfun(){
+	const text = document.getElementById("text").value;
+	const delay = document.getElementById("delay").value;
+	const output = document.getElementById("output");
+
+	await new Promise(resolve => setTimeout(resolve, delay))
+	output.innerText = text;
+}
